07bee1f6942d60251e3228fbc6c2467242fed707
F-Kirchgessner <florian.kirchgessner@tum.de>
2018-01-02 16:23:59 +0100

    Project basics

---
+++ src/classifiers/classification_cnn.py
@@ -0,0 +1,122 @@
+"""ClassificationCNN"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ClassificationCNN(nn.Module):
+    """
+    A PyTorch implementation of a three-layer convolutional network
+    with the following architecture:
+
+    conv - relu - 2x2 max pool - fc - dropout - relu - fc
+
+    The network operates on mini-batches of data that have shape (N, C, H, W)
+    consisting of N images, each with height H and width W and with C input
+    channels.
+    """
+
+    def __init__(self, input_dim=(3, 32, 32), num_filters=32, kernel_size=7,
+                 stride_conv=1, weight_scale=0.001, pool=2, stride_pool=2, hidden_dim=100,
+                 num_classes=10, dropout=0.0):
+        """
+        Initialize a new network.
+
+        Inputs:
+        - input_dim: Tuple (C, H, W) giving size of input data.
+        - num_filters: Number of filters to use in the convolutional layer.
+        - kernel_size: Size of filters to use in the convolutional layer.
+        - hidden_dim: Number of units to use in the fully-connected hidden layer-
+        - num_classes: Number of scores to produce from the final affine layer.
+        - stride_conv: Stride for the convolution layer.
+        - stride_pool: Stride for the max pooling layer.
+        - weight_scale: Scale for the convolution weights initialization
+        - pool: The size of the max pooling window.
+        - dropout: Probability of an element to be zeroed.
+        """
+        super(ClassificationCNN, self).__init__()
+        channels, height, width = input_dim
+
+        ########################################################################
+        # TODO: Initialize the necessary trainable layers to resemble the      #
+        # ClassificationCNN architecture  from the class docstring.            #
+        #                                                                      #
+        # In- and output features should not be hard coded which demands some  #
+        # calculations especially for the input of the first fully             #
+        # convolutional layer.                                                 #
+        #                                                                      #
+        # The convolution should use "same" padding which can be derived from  #
+        # the kernel size and its weights should be scaled. Layers should have #
+        # a bias if possible.                                                  #
+        #                                                                      #
+        # Note: Avoid using any of PyTorch's random functions or your output   #
+        # will not coincide with the Jupyter notebook cell.                    #
+        ########################################################################
+        self.m_pool = nn.MaxPool2d(pool, stride_pool)
+        self.m_dropout = dropout
+        padding = int((kernel_size - 1) / 2)
+        self.conv1 = nn.Conv2d(channels, num_filters, kernel_size, stride_conv, padding)
+        self.conv1.weight.data.mul_(weight_scale)
+
+        widthOut = int((width - kernel_size + 2 * padding) / stride_conv) + 1
+        heightOut = int((height - kernel_size + 2 * padding) / stride_conv) + 1
+        self.fc1 = nn.Linear(int(widthOut / 2) * int(heightOut / 2) * num_filters, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, num_classes)
+        ############################################################################
+        #                             END OF YOUR CODE                             #
+        ############################################################################
+
+    def forward(self, x):
+        """
+        Forward pass of the neural network. Should not be called manually but by
+        calling a model instance directly.
+
+        Inputs:
+        - x: PyTorch input Variable
+        """
+
+        ########################################################################
+        # TODO: Chain our previously initialized fully-connected neural        #
+        # network layers to resemble the architecture drafted in the class     #
+        # docstring. Have a look at the Variable.view function to make the     #
+        # transition from the spatial input image to the flat fully connected  #
+        # layers.                                                              #
+        ########################################################################
+        x = self.m_pool(F.relu(self.conv1(x)))
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(F.dropout(self.fc1(x), self.m_dropout))
+        x = self.fc2(x)
+        ########################################################################
+        #                             END OF YOUR CODE                         #
+        ########################################################################
+
+        return x
+
+    def num_flat_features(self, x):
+        """
+        Computes the number of features if the spatial input x is transformed
+        to a 1D flat input.
+        """
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+    @property
+    def is_cuda(self):
+        """
+        Check if model parameters are allocated on the GPU.
+        """
+        return next(self.parameters()).is_cuda
+
+    def save(self, path):
+        """
+        Save model with its parameters to the given path. Conventionally the
+        path should end with "*.model".
+
+        Inputs:
+        - path: path string
+        """
+        print('Saving model... %s' % path)
+        torch.save(self, path)
---
+++ src/solver.py
@@ -0,0 +1,146 @@
+from random import shuffle
+import numpy as np
+
+import torch
+from torch.autograd import Variable
+
+
+class Solver(object):
+    default_adam_args = {"lr": 1e-4,
+                         "betas": (0.9, 0.999),
+                         "eps": 1e-8,
+                         "weight_decay": 0.0}
+
+    def __init__(self, optim=torch.optim.Adam, optim_args={},
+                 loss_func=torch.nn.CrossEntropyLoss()):
+        optim_args_merged = self.default_adam_args.copy()
+        optim_args_merged.update(optim_args)
+        self.optim_args = optim_args_merged
+        self.optim = optim
+        self.loss_func = loss_func
+
+        self._reset_histories()
+
+    def _reset_histories(self):
+        """
+        Resets train and val histories for the accuracy and the loss.
+        """
+        self.train_loss_history = []
+        self.train_acc_history = []
+        self.val_acc_history = []
+        self.val_loss_history = []
+
+    def train(self, model, train_loader, val_loader, num_epochs=10, log_nth=0):
+        """
+        Train a given model with the provided data.
+
+        Inputs:
+        - model: model object initialized from a torch.nn.Module
+        - train_loader: train data in torch.utils.data.DataLoader
+        - val_loader: val data in torch.utils.data.DataLoader
+        - num_epochs: total number of training epochs
+        - log_nth: log training accuracy and loss every nth iteration
+        """
+        optim = self.optim(model.parameters(), **self.optim_args)
+        criterion = torch.nn.CrossEntropyLoss()
+        self._reset_histories()
+        iter_per_epoch = len(train_loader)
+
+        if torch.cuda.is_available():
+            model.cuda()
+
+        print('START TRAIN.')
+        ########################################################################
+        # TODO:                                                                #
+        # Write your own personal training method for our solver. In each      #
+        # epoch iter_per_epoch shuffled training batches are processed. The    #
+        # loss for each batch is stored in self.train_loss_history. Every      #
+        # log_nth iteration the loss is logged. After one epoch the training   #
+        # accuracy of the last mini batch is logged and stored in              #
+        # self.train_acc_history. We validate at the end of each epoch, log    #
+        # the result and store the accuracy of the entire validation set in    #
+        # self.val_acc_history.                                                #
+        #                                                                      #
+        # Your logging could like something like:                              #
+        #   ...                                                                #
+        #   [Iteration 700/4800] TRAIN loss: 1.452                             #
+        #   [Iteration 800/4800] TRAIN loss: 1.409                             #
+        #   [Iteration 900/4800] TRAIN loss: 1.374                             #
+        #   [Epoch 1/5] TRAIN acc/loss: 0.560/1.374                            #
+        #   [Epoch 1/5] VAL   acc/loss: 0.539/1.310                            #
+        #   ...                                                                #
+        ########################################################################
+        num_iterations = num_epochs * iter_per_epoch
+
+        for epoch in range(num_epochs):
+            correct = 0
+            total = 0
+            for i, data in enumerate(train_loader, 0):
+                # get the inputs
+                inputs, labels = data
+
+                # wrap them in Variable
+                inputs, labels = Variable(inputs), Variable(labels.type(torch.LongTensor))
+
+                if model.is_cuda:
+                    inputs = inputs.cuda()
+                    labels = labels.cuda()
+
+                # zero the parameter gradients
+                optim.zero_grad()
+
+                # forward + backward + optimize
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optim.step()
+
+                # print statistics
+                if i % 50 == 50 - 1:
+                    self.train_loss_history.append(loss.data[0])
+
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += torch.sum(predicted == labels.data)
+                    train_acc = 100*correct/total
+
+                    self.train_acc_history.append(train_acc)
+
+                    print('[%d, %5d] training:   loss: %.3f,  acc: %.3f' %(epoch + 1, i + 1, loss.data[0], train_acc))
+
+
+            # Validation
+            correctVal = 0
+            totalVal = 0
+            runningLossVal = 0.0
+            for i, data in enumerate(val_loader, 0):
+                # get the inputs
+                inputs, labels = data
+
+                # wrap them in Variable
+                inputs, labels = Variable(inputs), Variable(labels.type(torch.LongTensor))
+
+                if model.is_cuda:
+                    inputs = inputs.cuda()
+                    labels = labels.cuda()
+
+                # forward + backward + optimize
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                runningLossVal += loss.data[0]
+
+                _, predicted = torch.max(outputs.data, 1)
+                totalVal += labels.size(0)
+                correctVal += torch.sum(predicted == labels.data)
+                trainAccVal = 100*correctVal/totalVal
+
+            # print statistics
+            self.val_loss_history.append(runningLossVal)
+            self.val_acc_history.append(trainAccVal)
+            print('[%d, %5d] validation: loss: %.3f, acc: %.3f' %(epoch + 1, i + 1, runningLossVal, trainAccVal))
+
+            
+        ########################################################################
+        #                             END OF YOUR CODE                         #
+        ########################################################################
+        print('FINISH.')
